910768de170c646159608acd932824743adb636a
rdhruvramkhe-cmyk <rdhruvramkhe@gmail.com>
2026-04-23 11:51:55 +0100

    Delete smart-campus-api directory

--- smart-campus-api/src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.smartcampus.exception;
-
-// thrown when a sensor references a roomId that doesnt exist
-public class LinkedResourceNotFoundException extends RuntimeException {
-    public LinkedResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- smart-campus-api/src/main/java/com/smartcampus/exception/SensorUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.smartcampus.exception;
-
-// thrown when trying to post a reading to a sensor thats in MAINTENANCE
-public class SensorUnavailableException extends RuntimeException {
-    public SensorUnavailableException(String message) {
-        super(message);
-    }
-}

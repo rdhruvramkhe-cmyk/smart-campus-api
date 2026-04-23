5241ffc42178e874bc0be61aa3e0d53d81d2e4a3
rdhruvramkhe-cmyk <rdhruvramkhe@gmail.com>
2026-04-23 11:54:46 +0100

    Delete src/main/java/com/smartcampus directory

--- src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
+++
@@ -1,8 +0,0 @@
-package com.smartcampus.exception;
-
-// thrown when trying to delete a room that still has sensors
-public class RoomNotEmptyException extends RuntimeException {
-    public RoomNotEmptyException(String message) {
-        super(message);
-    }
-}
